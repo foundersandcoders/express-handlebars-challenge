80b6457a858d6fb2e59c16900d06926fc5a6da6a
PiotrBerebecki <piotr.berebecki@dazn.com>
2017-08-05 13:45:06 +0100

    Setup handlebars

---
+++ README.md
@@ -0,0 +1,1 @@
+# Express Handlebars Challenge
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ data: 'router' });
+  res.render('home', { users: ['user1', 'user2', 'user3'] });
 });
 
 module.exports = router;
